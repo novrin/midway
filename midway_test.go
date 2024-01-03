5f30a3d13eed3455e4e7f6dea1a24cddd35c89c1
novrin <106842821+novrin@users.noreply.github.com>
2024-01-02 20:16:58 -0800

    Clean up tests

--- midway_test.go
+++ midway_test.go
@@ -8,42 +8,40 @@ import (
 
 const errorString = "\nGot:\t%v\nWant:\t%v\n"
 
-func show(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("core"))
-}
-
-// sign is a Middleware that writes msg into ResponseWriter. In this testing
-// context, it is used to reveal the sequence of middleware execution.
-func sign(msg string) Middleware {
-	return func(h http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte(msg))
-			h.ServeHTTP(w, r)
-		})
-	}
-}
-
 func TestQueueAndStack(t *testing.T) {
+	base := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("core"))
+	})
+	// sign is a Middleware that writes msg into w. In this testing context, it
+	// is used to reveal the sequence of middleware execution.
+	sign := func(msg string) Middleware {
+		return func(h http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte(msg))
+				h.ServeHTTP(w, r)
+			})
+		}
+	}
 	ms := []Middleware{sign("m1_"), sign("m2_")}
 	cases := map[string]struct {
 		handler http.Handler
 		want    string
 	}{
 		"queue": {
-			handler: Queue(ms...)(http.HandlerFunc(show)),
+			handler: Queue(ms...)(base),
 			want:    "m2_m1_core",
 		},
 		"stack": {
-			handler: Stack(ms...)(http.HandlerFunc(show)),
+			handler: Stack(ms...)(base),
 			want:    "m1_m2_core",
 		},
 	}
-	for name, tc := range cases {
+	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
 			w := httptest.NewRecorder()
-			tc.handler.ServeHTTP(w, httptest.NewRequest(http.MethodGet, "/", nil))
-			if got := w.Body.String(); got != tc.want {
-				t.Errorf(errorString, got, tc.want)
+			c.handler.ServeHTTP(w, httptest.NewRequest(http.MethodGet, "/", nil))
+			if got := w.Body.String(); got != c.want {
+				t.Errorf(errorString, got, c.want)
 			}
 		})
 	}
